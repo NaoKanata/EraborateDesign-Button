2f4084b6ad3ef8df49dd4eaf73932c1024ebdc7e
NaoKanata <kanatanao7o@gmail.com>
2019-03-30 21:04:27 +0900

    init commit

---
+++ Assets/Scripts/PushEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushEffect : MonoBehaviour
+{
+    Vector2 oPos;
+    RectTransform rt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rt = GetComponent<RectTransform>();
+        oPos = rt.anchoredPosition;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MovePos(int y)
+    {
+        rt.anchoredPosition = new Vector2(rt.anchoredPosition.x,oPos.y+y);
+    }
+}
