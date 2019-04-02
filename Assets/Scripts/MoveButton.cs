20f2a94a54a712c142209a1e2f74a38846857de7
NaoKanata <kanatanao7o@gmail.com>
2019-04-02 16:04:58 +0900

    Completed Button UI Project

---
+++ Assets/Scripts/MoveButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveButton: MonoBehaviour
+{
+    [SerializeField]
+    Image img;
+    [SerializeField]
+    Image faceimg;
+
+    float f = 100.0f;
+    float fmax = 126.0f;
+    bool ff = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ff)
+        {
+            f -= f/4.0f;
+        }
+        else
+        {
+            f += (100.0f-f)/4.0f;
+        }
+        faceimg.color = new Color(1,1,1,f);
+        img.rectTransform.sizeDelta = new Vector2(292,f*1.26f);
+    }
+
+    public void Down(bool flag)
+    {
+        ff = flag;
+    }
+}
